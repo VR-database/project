bdd686b1253f5c2ee37ac841d2bbd400f3a87c7e
Willyamdevon <dezhintern@gmail.com>
2024-03-30 21:02:13 +0300

    preSetUo server-side

---
+++ server/app.py
@@ -0,0 +1,71 @@
+import uuid
+import psycopg2
+from psycopg2 import extras, Error
+from flask import Flask, jsonify, request, session, make_response
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['PERMANENT_SESSION_LIFETIME'] = 3600
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = "None"
+
+CORS(app, resources={r"*": {"origins": "http://localhost:5173", 'supports_credentials': True}})
+
+def db_get():
+    try:
+        pg = psycopg2.connect("""
+            host=localhost
+            dbname=postgres
+            user=postgres
+            password=kos120675
+            port=5432
+        """)
+
+        cursor = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        
+        pg.commit()
+
+        cursor.execute("SELECT * FROM users")
+        result = cursor.fetchall()
+
+        return_data = []
+        for row in result:
+            return_data.append(dict(row))
+
+    except (Exception, Error) as error:
+        return_data = f"Ошибка получения данных: {error}" 
+
+    finally:
+        if pg:
+            cursor.close
+            pg.close
+            print("Соединение с PostgreSQL закрыто")
+            return return_data
+        
+
+def login_user(pas):
+        
+    if pas=='kodvfwdse': return_data = 'Добро пожаловать!'
+    elif pas=='fdwji': return_data = 'Добро пожаловать, админ!'
+    else: return_data = "Неверный пароль!"
+    return return_data
+
+        
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        print(login_user(post_data.get('password')))
+        return jsonify(response_object)
+        
+    else:
+        response_object['message'] = db_get()
+        return jsonify(response_object)
+
+if __name__ == '__main__':
+    app.run()
